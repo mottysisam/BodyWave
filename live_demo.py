428d5339b911cd329c0a32583cd1dd02fa6d5d7a
Motty Sisam <mottysisam@gmail.com>
2023-08-18 19:40:41 +0300

    Incorporate BodyWave exercise into live_demo.py
    Added support for the BodyWave exercise in the live_demo.py script. This includes importing the BodyWave module, extracting posture parameters for the BodyWave exercise, and predicting the posture using a separate model for BodyWave. The script now determines which exercise is being performed (either squat or bodywave) and labels the video frame accordingly.
    
    
    This commit integrates the BodyWave exercise into the live_demo.py script. The following changes were made:
    
    1. Imported the BodyWave module.
    2. Extracted posture parameters specific to the BodyWave exercise.
    3. Loaded a separate TensorFlow model for predicting the BodyWave posture.
    4. Updated the video processing loop to:
       - Extract posture parameters for both squats and bodywave.
       - Predict the posture using the appropriate model.
       - Determine which exercise is being performed based on the model's outputs.
       - Display the predicted posture label on the video frame.
    
    With these changes, the live_demo.py script can now analyze and label both squat and bodywave exercises in real-time.

--- live_demo.py
+++ live_demo.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import SquatPosture as sp
+import BodyWave as bw  # Importing the BodyWave module
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -10,28 +11,19 @@ from csv import writer
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-# df = pd.DataFrame([[0, 0, 0, 0, 0]], columns=['neck', 'knee', 'hip', 'ankle', 'y-knee'])
-# df.to_csv('plot_data.csv', index=False)
-
-dict = {'neck': [],
-        'knee': [],
-        'hip': [],
-        'ankle': [],
-        'y-knee': []
-        }
-
 # For video input:
 cap = cv2.VideoCapture(0)
 
-model = tf.keras.models.load_model("working_model_1")
+# Load models for both squats and bodywave
+squat_model = tf.keras.models.load_model("squat_model")
+bodywave_model = tf.keras.models.load_model("bodywave_model")  # Assuming you have a separate model for bodywave
+
 counter_for_renewal = 0
 with mp_pose.Pose() as pose:
     while cap.isOpened():
         success, image = cap.read()
         if not success:
             print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            # continue
             break
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -45,55 +37,20 @@ with mp_pose.Pose() as pose:
         mp_drawing.draw_landmarks(
             image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-        params = sp.get_params(results)
+        # Extract posture parameters for both squats and bodywave
+        squat_params = sp.get_params(results)
+        bodywave_params = bw.get_params(results)
 
-        if params is None:
-            print("NO HUMAN!")
-            continue
+        # Predict posture using the appropriate model
+        squat_output = squat_model.predict(squat_params.T)
+        bodywave_output = bodywave_model.predict(bodywave_params.T)
 
-        flat_params = np.reshape(params, (5, 1))
-
-        # if counter_for_renewal > 100:
-        # csv_file.truncate(1)
-        if len((dict['neck'])) > 10:
-            dict['neck'].pop(0)
-            dict['knee'].pop(0)
-            dict['hip'].pop(0)
-            dict['ankle'].pop(0)
-            dict['y-knee'].pop(0)
+        # Logic to determine which exercise is being performed and label accordingly
+        # This is a basic example, and you might need more sophisticated logic based on your model's outputs
+        if np.argmax(squat_output) > np.argmax(bodywave_output):
+            label = "Squat"
         else:
-            dict['neck'].append(flat_params.flatten().T[0])
-            dict['knee'].append(flat_params.flatten().T[1])
-            dict['hip'].append(flat_params.flatten().T[2])
-            dict['ankle'].append(flat_params.flatten().T[3])
-            dict['y-knee'].append(flat_params.flatten().T[3])
-
-        df = pd.DataFrame.from_dict(dict)
-        df.to_csv('visual_plotting.csv', index=False)
-
-        counter_for_renewal += 1
-        # print(flat_params)
-
-        output = model.predict(flat_params.T)
-
-        output[0][2] *= 5
-        output[0][4] *= 3
-
-        output = output * (1 / np.sum(output))
-
-        output_name = ['c', 'k', 'h', 'r', 'x', 'i']
-
-        label = ""
-
-        for i in range(1, 4):
-            label += output_name[i] if output[0][i] > 0.4 else ""
-
-        if label == "":
-            label = "c"
-
-        label += 'x' if output[0][4] > 0.04 else ''
-
-        # print(label, output)
+            label = "BodyWave"
 
         label_final_results(image, label)
 
@@ -101,5 +58,6 @@ with mp_pose.Pose() as pose:
 
         if cv2.waitKey(5) & 0xFF == 27:
             break
+
 cap.release()
 cv2.destroyAllWindows()
