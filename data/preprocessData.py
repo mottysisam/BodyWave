a853b2d3ca96ee0c608eb0d2f600baa64037cde6
Motty Sisam <mottysisam@gmail.com>
2023-08-18 19:13:26 +0300

    adding bodywave into preprocess data
    Changes made:
    
    Added a directory path for 'bodywave' in the preprocess function.
    Modified the main execution logic to loop through each exercise type and process the videos accordingly. This makes the code more scalable and easier to add more exercises in the future.

--- data/preprocessData.py
+++ data/preprocessData.py
@@ -7,36 +7,49 @@ import pickle
 def preprocess(video, output, fps=12, width=720, height=1280, exercise='squats'):
     stream = ffmpeg.input(video)
     stream = stream.filter('fps', fps=fps, round='up')
-    if exercise=='squats':
-        out = "processed/"+output
-    elif exercise=='plank':
-        out = "plank_processed/"+output
+    
+    if exercise == 'squats':
+        out = "processed/" + output
+    elif exercise == 'plank':
+        out = "plank_processed/" + output
+    elif exercise == 'bodywave':  # Adding support for bodywave
+        out = "bodywave_processed/" + output
 
     stream = ffmpeg.output(stream, out)
-
     ffmpeg.run(stream, quiet=True)
 
-
 if __name__ == "__main__":
-    processed = sorted(os.listdir("plank_processed"))
-    raw = sorted(os.listdir("plank_raw"))
+    # Assuming you have a directory for bodywave_raw and bodywave_processed
+    exercises = {
+        'plank': {
+            'raw': 'plank_raw',
+            'processed': 'plank_processed'
+        },
+        'bodywave': {  # Adding support for bodywave
+            'raw': 'bodywave_raw',
+            'processed': 'bodywave_processed'
+        }
+    }
 
-    if len(processed) == 0:
-        count = 0
-    else:
-        count = int(processed[-1][:3])
-        count += 1
+    for exercise, paths in exercises.items():
+        processed = sorted(os.listdir(paths['processed']))
+        raw = sorted(os.listdir(paths['raw']))
 
-    print(count)
+        if len(processed) == 0:
+            count = 0
+        else:
+            count = int(processed[-1][:3])
+            count += 1
 
-    for i in raw:
-        file = f"plank_raw/{i}"
-        leading_count = str(count).zfill(3)
-        name = leading_count+"_plank.mp4"
-        preprocess(file, name)
+        print(count)
 
-        count+=1
-        print(name)
+        for i in raw:
+            file = f"{paths['raw']}/{i}"
+            leading_count = str(count).zfill(3)
+            name = f"{leading_count}_{exercise}.mp4"
+            preprocess(file, name, exercise=exercise)
 
-        os.remove(file)
-        # break
+            count += 1
+            print(name)
+
+            os.remove(file)
